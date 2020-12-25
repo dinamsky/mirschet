21bcac66336e87bd69771ed207ddaa643e2cde27
Igor Dinamsky <dinamsky@gmail.com>
2020-12-25 11:49:18 +0300

    initial commit

---
+++ wa-config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return array (
+  'default' => 
+  array (
+    'host' => 'localhost',
+    'port' => false,
+    'user' => 'u1240732_mir',
+    'password' => 'C8z0J4h1',
+    'database' => 'u1240732_mir',
+    'type' => 'mysqli',
+    'sql_mode' => 'TRADITIONAL',
+  ),
+);
+//EOF
